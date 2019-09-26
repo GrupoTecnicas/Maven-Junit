d939c1adcca527db008a368734c6329e4aaff893
Frederico Martins <frederico.martins@edu.pucrs.br>
2019-09-26 11:11:48 -0300

    Testes prontos

--- projeto-teste/src/test/java/com/exercicioteste/FuncionarioTest.java
+++ projeto-teste/src/test/java/com/exercicioteste/FuncionarioTest.java
@@ -1,6 +1,5 @@
 package com.exercicioteste;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,10 +8,32 @@ public class FuncionarioTest {
 
     private Funcionario f;
 
+    @BeforeEach
+    public void setup(){
+        f = new Funcionario();
+    }
+
     @Test
-    public void setSalarioBrutoTest(double salarioBruto){
-        f.setSalarioBruto(salarioBruto);
-        assertEquals(salarioBruto, f.getSalarioBruto());
+    public void testINSSmenorLimite(){
+        f.setSalarioBruto(3000.0);
+        double rEsp = 3000.0*0.045;
+        double rObs = f.getINSS();
+        assertEquals(rEsp,rObs,0.0001);
     }
     
+    @Test
+    public void testINSSigualLimite(){
+        f.setSalarioBruto(5000.0);
+        double rEsp = 5000.0*0.045;
+        double rObs = f.getINSS();
+        assertEquals(rEsp,rObs,0.0001);
+    }
+
+    @Test
+    public void testIRmaior(){
+        f.setSalarioBruto(3000.0);
+        double rEsp = 3000.0 * 0.12;
+        double rObs = f.getIRPF();
+        assertEquals(rEsp, rObs, 0.0001);
+    }
 }
